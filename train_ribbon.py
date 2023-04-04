8333ec0fae7a9f206db6290a5fa7a98f46314c8a
ParkSungHyon <shp234234@gmail.com>
2023-04-04 14:41:51 +0900

    Update train_ribbon.py

--- train_ribbon.py
+++ train_ribbon.py
@@ -18,10 +18,10 @@ import os
 from detectron2.data.datasets import register_coco_instances
 
 # 경로 설정
-train_json_path = "C:/python123/r_cnn_ribbon/ribbon/train/train_json"
-train_img_path = "C:/python123/r_cnn_ribbon/ribbon/train/train_img"
-val_json_path = "C:/python123/r_cnn_ribbon/ribbon/val/val_json"
-val_img_path = "C:/python123/r_cnn_ribbon/ribbon/val/val_img"
+train_json_path = "/Users/b31/Documents/ribbon/train/train_json"
+train_img_path = "/Users/b31/Documents/ribbon/train/train_img"
+val_json_path = "/Users/b31/Documents/ribbon/val/val_json"
+val_img_path = "/Users/b31/Documents/ribbon/val/val_img"
 
 # train_json_path 폴더에서 모든 .json 파일 검색
 train_json_files = [f for f in os.listdir(train_json_path) if f.endswith('.json')]
@@ -39,7 +39,7 @@ for val_json_file in val_json_files:
 
 # Load the pre-trained model
 cfg = get_cfg()
-cfg.merge_from_file("C:/python123/r_cnn_ribbon/faster_rcnn_R_50_FPN_3x.yaml")
+cfg.merge_from_file("/Users/b31/Documents/r_cnn_ribbon2/faster_rcnn_R_50_FPN_3x.yaml")
 
 # Dataset settings
 cfg.DATASETS.TRAIN = (train_dataset_name,)
@@ -49,13 +49,7 @@ cfg.DATASETS.TEST = ()
 # Dataloader settings
 cfg.DATALOADER.NUM_WORKERS = 2
 
-# Trainer settings
-cfg.SOLVER.IMS_PER_BATCH = 2
-cfg.SOLVER.BASE_LR = 0.00025
-cfg.SOLVER.MAX_ITER = 5000
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # Only one class: ribbon
-cfg.OUTPUT_DIR = "C:/python123/r_cnn_ribbon2"
+cfg.OUTPUT_DIR = "/Users/b31/Documents/r_cnn_ribbon2/result"
 
 # Train the model
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
@@ -64,11 +58,11 @@ trainer.resume_or_load(resume=False)
 trainer.train()
 
 # Evaluate the model on the validation dataset
-evaluator = COCOEvaluator("ribbon_val", cfg, False, output_dir=cfg.OUTPUT_DIR)
-val_loader = trainer.data_loader.build_test_loader(cfg, "ribbon_val")
+evaluator = COCOEvaluator(val_dataset_name, cfg, False, output_dir=cfg.OUTPUT_DIR)
+val_loader = trainer.data_loader.build_test_loader(cfg, val_dataset_name)
 inference_on_dataset(trainer.model, val_loader, evaluator)
 
 # Save the trained model as Ribbon.pth
 trained_model_path = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-target_model_path = os.path.join(cfg.OUTPUT_DIR, "Ribbon.pth")
+target_model_path = os.path.join(cfg.OUTPUT_DIR, "ribbon_detect.pth")
 shutil.move(trained_model_path, target_model_path)
